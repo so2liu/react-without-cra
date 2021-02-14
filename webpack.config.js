dac38f4834621b69dd3b462fe865c134dfd74fce
刘扬 <liuyang02@4paradigm.com>
2021-02-14 21:47:31 +0800

    feat: able to run

--- webpack.config.js
+++ webpack.config.js
@@ -13,4 +13,18 @@ module.exports = {
             filename: "index.html",
         }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    },
+                },
+            },
+        ],
+    },
 };

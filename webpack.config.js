50373f6e2fa846514e1b0baf2f2b62d565615359
刘扬 <liuyang02@4paradigm.com>
2021-02-14 21:34:54 +0800

    feat: html template

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,10 @@ module.exports = {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
     },
-    plugins: [new HtmlWebpackPlugin()],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "public/index.html"),
+            filename: "index.html",
+        }),
+    ],
 };
